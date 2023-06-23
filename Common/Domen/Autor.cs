97063d9b5a88b36586d354a8b53d5987f46947fd
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-23 21:01:12 +0200

    Dodate klase Autor i Biblioteka

---
+++ Common/Domen/Autor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class Autor : IEntitet
+    {
+        [Browsable(false)]
+        public int IDAutor { get; set; }
+
+        public string ImePrezime { get; set; }
+
+        public Biblioteka Biblioteka { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ImePrezime}";
+        }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+        [Browsable(false)]
+        public string ImeTabele => "Autor";
+        [Browsable(false)]
+        public string UbaciVrednosti => "";
+        [Browsable(false)]
+        public string IdName => "IDAutor";
+        [Browsable(false)]
+        public string JoinUslov => "join Biblioteka b on (b.IDBiblioteka = a.IDBiblioteka)";
+        [Browsable(false)]
+        public string Alias => "a";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => "";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+
+            while (reader.Read())
+            {
+                entiteti.Add(new Autor
+                {
+                    IDAutor = (int)reader[0],
+                    ImePrezime = (string)reader[1],
+                    Biblioteka = new Biblioteka
+                    {
+                        IDBiblioteka = (int)reader[3],
+                        Ime = (string)reader[4],
+                        Adresa = (string)reader[5],
+                    }
+                });
+            }
+            return entiteti;
+        }
+    }
+}
---
+++ Common/Domen/Biblioteka.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class Biblioteka : IEntitet
+    {
+        [Browsable(false)]
+        public int IDBiblioteka { get; set; }
+
+        public string Ime { get; set; }
+
+        public string Adresa { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Ime}";
+        }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+        [Browsable(false)]
+        public string ImeTabele => "Biblioteka";
+        [Browsable(false)]
+        public string UbaciVrednosti => "";
+        [Browsable(false)]
+        public string IdName => "IDBiblioteka";
+        [Browsable(false)]
+        public string JoinUslov => "";
+        [Browsable(false)]
+        public string Alias => "b";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => "";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+
+            while (reader.Read())
+            {
+                entiteti.Add(new Biblioteka
+                {
+                    IDBiblioteka = (int)reader[0],
+                    Ime = (string)reader[1],
+                    Adresa = (string)reader[2]
+                });
+            }
+            return entiteti;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Biblioteka other = (Biblioteka)obj;
+
+            return IDBiblioteka == other.IDBiblioteka;
+        }
+    }
+}
