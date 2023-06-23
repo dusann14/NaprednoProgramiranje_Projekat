be1ee6519792ef348c7c71f625fb8d5aa2849464
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:24:53 +0200

    Dodat repozotorijum koji ima referencu na brokera baze koji komunicira sa bazom

---
+++ Common/Baza/GenerickiRepo.cs
@@ -0,0 +1,68 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Baza
+{
+    public class GenerickiRepo : IGenerickiRepo
+    {
+        private BrokerBaze broker = new BrokerBaze();
+        public void Commit()
+        {
+            broker.Commit();
+        }
+
+        public void OtvoriKonekciju()
+        {
+            broker.OtvoriKonekciju();
+        }
+
+        public void PokreniTransakciju()
+        {
+            broker.PokreniTransakciju();
+        }
+
+        public void Rollback()
+        {
+            broker.Rollback();
+        }
+
+        public void ZatvoriKonekciju()
+        {
+            broker.ZatvoriKonekciju();
+        }
+
+        public IEntitet PronadjiJednog(IEntitet entitet)
+        {
+            return broker.PronadjiJednog(entitet);
+        }
+
+        public List<IEntitet> VratiSve(IEntitet entitet)
+        {
+            return broker.VratiSve(entitet);
+        }
+
+        public List<IEntitet> VratiPoUslovu(IEntitet entitet)
+        {
+            return broker.PretraziTabele(entitet);
+        }
+
+        public int Dodaj(IEntitet entitet)
+        {
+            return broker.Dodaj(entitet);
+        }
+
+        public void Obrisi(IEntitet entitet)
+        {
+            broker.Obrisi(entitet);
+        }
+
+        public void Promeni(IEntitet entitet)
+        {
+            broker.Promeni(entitet);
+        }
+    }
+}
---
+++ Common/Baza/IGenerickiRepo.cs
@@ -0,0 +1,29 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Baza
+{
+    public interface IGenerickiRepo
+    {
+        void Promeni(IEntitet entitet);
+        void Obrisi(IEntitet entitet);
+        int Dodaj(IEntitet entitet);
+        List<IEntitet> VratiSve(IEntitet entitet);
+
+        List<IEntitet> VratiPoUslovu(IEntitet entitet);
+        IEntitet PronadjiJednog(IEntitet entitet);
+        void OtvoriKonekciju();
+
+        void ZatvoriKonekciju();
+
+        void PokreniTransakciju();
+
+        void Commit();
+
+        void Rollback();
+    }
+}
