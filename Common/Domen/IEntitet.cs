b2a6cb496eb8852223a6756753ffd98cb63a85d4
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-23 20:59:54 +0200

    Dodat interfejs IEntitet kojeg sve domenske klase implementiraju

---
+++ Common/Domen/IEntitet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    public interface IEntitet
+    {
+        string ImeTabele { get; }
+        string UbaciVrednosti { get; }
+        string IdName { get; }
+        string JoinUslov { get; }
+        string Alias { get; }
+        string Select { get; }
+        string WhereUslov { get; }
+
+        string UpdateVrednosti { get; }
+
+        List<IEntitet> VratiVise(SqlDataReader reader);
+
+        IEntitet VratiJednog(SqlDataReader reader);
+    }
+}
