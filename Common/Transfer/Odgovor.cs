f04338bcedc6f9bbb191188525de990ef01ce3b7
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:27:14 +0200

    Dodata klasa koja predstvalja serverski odgovor

---
+++ Common/Transfer/Odgovor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Transfer
+{
+    [Serializable]
+    public class Odgovor
+    {
+        public bool Uspesno { get; set; }
+
+        public string Greska { get; set; }
+        public object Rezultat { get; set; }
+    }
+}
