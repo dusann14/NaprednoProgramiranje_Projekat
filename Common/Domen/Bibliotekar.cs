5687ea2fa15bc7b256668193945f666a1904ba56
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-23 21:02:27 +0200

    Dodati klase Clan i Bibliotekar

---
+++ Common/Domen/Bibliotekar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class Bibliotekar : IEntitet
+    {
+        [Browsable(false)]
+        public int IDBibliotekar { get; set; }
+
+        public string ImePrezime { get; set; }
+
+        public string KorisnickoIme { get; set; }
+
+        public string Lozinka { get; set; }
+
+        public bool Prijavljen { get; set; }
+
+        public DateTime DatumRodjenja { get; set; }
+
+        public Biblioteka Biblioteka { get; set; }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+        [Browsable(false)]
+        public string ImeTabele => "Bibliotekar";
+        [Browsable(false)]
+        public string UbaciVrednosti => throw new NotImplementedException();
+        [Browsable(false)]
+        public string IdName => throw new NotImplementedException();
+        [Browsable(false)]
+        public string JoinUslov => "join Biblioteka b on (bib.IDBiblioteka = b.IDBiblioteka)";
+        [Browsable(false)]
+        public string Alias => "bib";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => $"ImePrezime = '{ImePrezime}', KorisnickoIme = '{KorisnickoIme}', Lozinka = '{Lozinka}', DatumRodjenja = '{DatumRodjenja}', IDBiblioteka = {Biblioteka.IDBiblioteka}, Prijavljen = '{Prijavljen}'";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            return new Bibliotekar
+            {
+                IDBibliotekar = (int)reader[0],
+                ImePrezime = (string)reader[1],
+                KorisnickoIme = (string)reader[2],
+                Lozinka = (string)reader[3],
+                Prijavljen = (bool)reader[4],
+                DatumRodjenja = (DateTime)reader[5],
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = (int)reader[7],
+                    Ime = (string)reader[8],
+                    Adresa = (string)reader[9],
+                }
+            };
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+            while (reader.Read())
+            {
+                entiteti.Add(new Bibliotekar
+                {
+                    IDBibliotekar = (int)reader[0],
+                    ImePrezime = (string)reader[1],
+                    KorisnickoIme = (string)reader[2],
+                    Lozinka = (string)reader[3],
+                    Prijavljen = (bool)reader[4],
+                    DatumRodjenja = (DateTime)reader[5],
+                    Biblioteka = new Biblioteka
+                    {
+                        IDBiblioteka = (int)reader[7],
+                        Ime = (string)reader[8],
+                        Adresa = (string)reader[9],
+                    }
+                });
+
+            }
+            return entiteti;
+        }
+    }
+}
---
+++ Common/Domen/Clan.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class Clan : IEntitet
+    {
+        [Browsable(false)]
+        public int IDClan { get; set; }
+
+
+        public string KorisnickoIme { get; set; }
+
+        public string Lozinka { get; set; }
+
+
+        public string ImePrezime { get; set; }
+
+        public bool Prijavljen { get; set; }
+
+
+        public DateTime DatumRodjenja { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ImePrezime}";
+        }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+        [Browsable(false)]
+        public string ImeTabele => "Clan";
+        [Browsable(false)]
+        public string UbaciVrednosti => throw new NotImplementedException();
+        [Browsable(false)]
+        public string IdName => throw new NotImplementedException();
+        [Browsable(false)]
+        public string JoinUslov => "";
+        [Browsable(false)]
+        public string Alias => "c";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => $"KorisnickoIme = '{KorisnickoIme}', Lozinka = '{Lozinka}', ImePrezime = '{ImePrezime}', Prijavljen = '{Prijavljen}', DatumRodjenja = '{DatumRodjenja}'";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            return new Clan
+            {
+                IDClan = (int)reader[0],
+                ImePrezime = (string)reader[1],
+                KorisnickoIme = (string)reader[2],
+                Lozinka = (string)reader[3],
+                Prijavljen = (bool)reader[4],
+                DatumRodjenja = (DateTime)reader[5],
+            };
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+            while (reader.Read())
+            {
+                entiteti.Add(new Clan
+                {
+                    IDClan = (int)reader[0],
+                    ImePrezime = (string)reader[1],
+                    KorisnickoIme = (string)reader[2],
+                    Lozinka = (string)reader[3],
+                    DatumRodjenja = (DateTime)reader[4],
+                    Prijavljen = (bool)reader[5]
+                });
+
+            }
+            return entiteti;
+        }
+    }
+}
