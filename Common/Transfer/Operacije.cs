8e1fc420d773c712527a858b5d81e2b501429675
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:26:27 +0200

    Dodata klasa koja predstavlja klijentski zahtev

---
+++ Common/Transfer/Operacije.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Transfer
+{
+    [Serializable]
+    public enum Operacije
+    {
+        VratiKnjigeIzBiblioteke,
+        VratiKnjigePoAutoru,
+        DodajKnjigu,
+        ObrisiKnjigu,
+        PromeniKnjigu,
+        PromeniPodatkeBibliotekara,
+        PrikaziClanovePoImenu,
+        PrikaziSveClanoveBiblioteke,
+        PrikaziSveRezervacije,
+        PromeniPodatkeClana,
+        KreirajRezervaciju,
+        VratiAutore,
+        VratiBibliotekeClana,
+        PrikaziSveBiblioteke,
+        OtkaziClanstvo,
+        UclaniSe,
+        VratiStavkeRezervacije,
+        VratiKnjigePoNaslovu,
+        VratiNeobradjeneRezervacije,
+        ObradiRezervaciju,
+        VratiObradjeneRezervacije
+    }
+}
---
+++ Common/Transfer/Zahtev.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Transfer
+{
+    [Serializable]
+    public class Zahtev
+    {
+        public object Objekat { get; set; }
+
+        public Operacije Operacija { get; set; }
+    }
+}
