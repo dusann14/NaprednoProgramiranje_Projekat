7abb68ba67ae6b9fea1a4874a87db2acc7e11d90
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:31:36 +0200

    Dodate sistemske operacije za bibliotekara

---
+++ Common/SistemskeOperacije/BibliotekarSO/PromeniPodatkeBibliotekaraSO.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije.BibliotekarSO
+{
+    public class PromeniPodatkeBibliotekaraSO : SistemskaOperacijaBaza
+    {
+        protected override void Izvrsi(IEntitet entitet)
+        {
+            Bibliotekar bibliotekar = (Bibliotekar)entitet;
+            bibliotekar.Uslov = $"IDBibliotekar = {bibliotekar.IDBibliotekar}";
+            repozitorijum.Promeni(bibliotekar);
+        }
+    }
+}
