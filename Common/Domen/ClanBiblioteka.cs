e0545d23db41572711aa2d3485ef0cdec51c9c07
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-23 21:03:41 +0200

    Dodate klase Knjiga i ClanBIblioteka

---
+++ Common/Domen/ClanBiblioteka.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class ClanBiblioteka : IEntitet
+    {
+        public DateTime DatumUclanjenja { get; set; }
+
+        public Biblioteka Biblioteka { get; set; }
+
+        public Clan Clan { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Clan}, {DatumUclanjenja}";
+        }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+
+        [Browsable(false)]
+        public string ImeTabele => "ClanBiblioteka";
+        [Browsable(false)]
+        public string UbaciVrednosti => $"'{DatumUclanjenja}', {Biblioteka.IDBiblioteka}, {Clan.IDClan}";
+        [Browsable(false)]
+        public string IdName => "IDClan";
+        [Browsable(false)]
+        public string JoinUslov => "join Biblioteka b on (b.IDBiblioteka = cb.IDBiblioteka) join Clan c on (c.IDClan = cb.IDClan)";
+        [Browsable(false)]
+        public string Alias => "cb";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => "";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            return new ClanBiblioteka
+            {
+                DatumUclanjenja = (DateTime)reader[0],
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = (int)reader[3],
+                    Ime = (string)reader[4],
+                    Adresa = (string)reader[5],
+                },
+                Clan = new Clan
+                {
+                    IDClan = (int)reader[6],
+                    ImePrezime = (string)reader[7],
+                    KorisnickoIme = (string)reader[8],
+                    Lozinka = (string)reader[9],
+                    Prijavljen = (bool)reader[10],
+                    DatumRodjenja = (DateTime)reader[11]
+                }
+            };
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+            while (reader.Read())
+            {
+                entiteti.Add(new ClanBiblioteka
+                {
+                    DatumUclanjenja = (DateTime)reader[0],
+                    Biblioteka = new Biblioteka
+                    {
+                        IDBiblioteka = (int)reader[3],
+                        Ime = (string)reader[4],
+                        Adresa = (string)reader[5],
+                    },
+                    Clan = new Clan
+                    {
+                        IDClan = (int)reader[6],
+                        ImePrezime = (string)reader[7],
+                        KorisnickoIme = (string)reader[8],
+                        Lozinka = (string)reader[9],
+                        Prijavljen = (bool)reader[10],
+                        DatumRodjenja = (DateTime)reader[11]
+                    }
+                });
+            }
+            return entiteti;
+        }
+    }
+}
---
+++ Common/Domen/Knjiga.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domen
+{
+    [Serializable]
+    public class Knjiga : IEntitet
+    {
+        [Browsable(false)]
+        public int IDKnjiga { get; set; }
+
+        public string Naslov { get; set; }
+
+        public int BrojPrimeraka { get; set; }
+
+        public Autor Autor { get; set; }
+
+        public Biblioteka Biblioteka { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Naslov}";
+        }
+
+        [Browsable(false)]
+        public string Uslov { get; set; }
+        [Browsable(false)]
+        public string ImeTabele => "Knjiga";
+        [Browsable(false)]
+        public string UbaciVrednosti => $"'{Naslov}', {BrojPrimeraka}, {Autor.IDAutor}, {Biblioteka.IDBiblioteka}";
+        [Browsable(false)]
+        public string IdName => "IDKnjiga";
+        [Browsable(false)]
+        public string JoinUslov => "join Autor a on (a.IDAutor = k.IDAutor) join Biblioteka b on (b.IDBiblioteka = k.IDBiblioteka)";
+        [Browsable(false)]
+        public string Alias => "k";
+        [Browsable(false)]
+        public string Select => "*";
+        [Browsable(false)]
+        public string WhereUslov => $"{Uslov}";
+        [Browsable(false)]
+        public string UpdateVrednosti => $"Naslov = '{Naslov}', BrojPrimeraka = {BrojPrimeraka}, IDAutor = {Autor.IDAutor}, IDBiblioteka = {Biblioteka.IDBiblioteka}";
+
+        public IEntitet VratiJednog(SqlDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IEntitet> VratiVise(SqlDataReader reader)
+        {
+            List<IEntitet> entiteti = new List<IEntitet>();
+            while (reader.Read())
+            {
+                entiteti.Add(new Knjiga
+                {
+                    IDKnjiga = (int)reader[0],
+                    Naslov = (string)reader[1],
+                    BrojPrimeraka = (int)reader[2],
+                    Autor = new Autor
+                    {
+                        IDAutor = (int)reader[5],
+                        ImePrezime = (string)reader[6],
+                        Biblioteka = new Biblioteka
+                        {
+                            IDBiblioteka = (int)reader[8],
+                            Ime = (string)reader[9],
+                            Adresa = (string)reader[10]
+                        }
+                    },
+                    Biblioteka = new Biblioteka
+                    {
+                        IDBiblioteka = (int)reader[8],
+                        Ime = (string)reader[9],
+                        Adresa = (string)reader[10]
+                    }
+                });
+            }
+            return entiteti;
+        }
+    }
+}
