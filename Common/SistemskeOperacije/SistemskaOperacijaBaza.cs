ce01b96c9597b802b0f7bee03ef05beebdf6b4ac
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:29:48 +0200

    Dodata sistemska operacija baza klasa koju sve so nasledjuju

---
+++ Common/SistemskeOperacije/SistemskaOperacijaBaza.cs
@@ -0,0 +1,42 @@
+﻿using Common.Baza;
+using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije
+{
+    public abstract class SistemskaOperacijaBaza
+    {
+        protected IGenerickiRepo repozitorijum;
+
+        public SistemskaOperacijaBaza()
+        {
+            repozitorijum = new GenerickiRepo();
+        }
+
+        public void Template(IEntitet entitet)
+        {
+            try
+            {
+                repozitorijum.OtvoriKonekciju();
+                repozitorijum.PokreniTransakciju();
+                Izvrsi(entitet);
+                repozitorijum.Commit();
+            }
+            catch (Exception e)
+            {
+                repozitorijum.Rollback();
+                throw e;
+            }
+            finally
+            {
+                repozitorijum.ZatvoriKonekciju();
+            }
+        }
+
+        protected abstract void Izvrsi(IEntitet entitet);
+    }
+}
