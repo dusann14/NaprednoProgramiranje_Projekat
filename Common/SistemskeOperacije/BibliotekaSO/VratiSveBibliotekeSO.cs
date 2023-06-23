bb5fc5fac1ca10e8613a0cb0f82b219d2cfd1eca
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:32:32 +0200

    Dodate sistemske operacije za biblioteku

---
+++ Common/SistemskeOperacije/BibliotekaSO/VratiSveBibliotekeSO.cs
@@ -0,0 +1,18 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije.BibliotekaSO
+{
+    public class VratiSveBibliotekeSO : SistemskaOperacijaBaza
+    {
+        public List<Biblioteka> Rezultat { get; set; }
+        protected override void Izvrsi(IEntitet entitet)
+        {
+            Rezultat = repozitorijum.VratiSve(entitet).Cast<Biblioteka>().ToList();
+        }
+    }
+}
