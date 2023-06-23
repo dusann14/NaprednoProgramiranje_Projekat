e91a2e7b2b822658d66c9abd7e8b33d44cc3b64c
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:38:51 +0200

    Dodate sistemske operacije za login

---
+++ Common/SistemskeOperacije/LoginSO/LoginSO.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije.LoginSO
+{
+    public class LoginSO : SistemskaOperacijaBaza
+    {
+        public IEntitet Rezultat { get; private set; }
+
+        protected override void Izvrsi(IEntitet entitet)
+        {
+            Rezultat = repozitorijum.PronadjiJednog(entitet);
+        }
+    }
+}
