79034b412927e7e82740856218f9908df420f218
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:30:40 +0200

    Dodate sistemske operacije za autora

---
+++ Common/SistemskeOperacije/AutorSO/VratiAutoreSO.cs
@@ -0,0 +1,20 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije.AutorSO
+{
+    public class VratiAutoreSO : SistemskaOperacijaBaza
+    {
+        public List<Autor> Rezultat { get; set; }
+        protected override void Izvrsi(IEntitet entitet)
+        {
+            Autor autor = (Autor)entitet;
+            autor.Uslov = $"a.IDBiblioteka = {autor.Biblioteka.IDBiblioteka}";
+            Rezultat = repozitorijum.VratiPoUslovu(autor).Cast<Autor>().ToList();
+        }
+    }
+}
