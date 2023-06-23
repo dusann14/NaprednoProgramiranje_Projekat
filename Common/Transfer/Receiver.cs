4bb80a846ac8aceb14f269fd466adef04cd287d1
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:28:22 +0200

    Dodate klase Sender i Receiver

---
+++ Common/Transfer/Receiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Transfer
+{
+    public class Receiver
+    {
+        private NetworkStream tok;
+        private BinaryFormatter formatter;
+
+        public Receiver(Socket soket)
+        {
+            tok = new NetworkStream(soket);
+            formatter = new BinaryFormatter();
+        }
+
+        public object Primi()
+        {
+            return formatter.Deserialize(tok);
+        }
+    }
+}
---
+++ Common/Transfer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Transfer
+{
+    public class Sender
+    {
+        private NetworkStream tok;
+        private BinaryFormatter formatter;
+
+        public Sender(Socket soket)
+        {
+            tok = new NetworkStream(soket);
+            formatter = new BinaryFormatter();
+        }
+
+        public void Posalji(object poruka)
+        {
+            formatter.Serialize(tok, poruka);
+        }
+    }
+}
