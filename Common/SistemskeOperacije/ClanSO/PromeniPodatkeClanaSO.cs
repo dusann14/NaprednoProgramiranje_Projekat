15c8f2f0c47d22fbf26238caaf4df59ccad9bd52
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 00:35:50 +0200

    Dodate sistemske operacije za clana

---
+++ Common/SistemskeOperacije/ClanSO/PromeniPodatkeClanaSO.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SistemskeOperacije.ClanSO
+{
+    public class PromeniPodatkeClanaSO : SistemskaOperacijaBaza
+    {
+        protected override void Izvrsi(IEntitet entitet)
+        {
+            Clan clan = (Clan)entitet;
+            clan.Uslov = $"IDClan = {clan.IDClan}";
+            repozitorijum.Promeni(clan);
+        }
+    }
+}
