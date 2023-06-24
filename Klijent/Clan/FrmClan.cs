78bcb091982d1045ee9c8f3f14b3ce6513ca3064
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-06-24 14:59:00 +0200

    Dodata glavna forma za clana

---
+++ Klijent/Clan/FrmClan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klijent.Clan
+{
+    public partial class FrmClan : Form
+    {
+        public FrmClan()
+        {
+            InitializeComponent();
+        }
+
+        internal void SetPanel(UserControl userControl)
+        {
+            panel1.Controls.Clear();
+            userControl.Parent = panel1;
+            userControl.Dock = DockStyle.Fill;
+        }
+
+    }
+}
