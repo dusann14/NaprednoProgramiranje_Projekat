f68621184c27c2eb9d955c3a36042e8e2b899d4c
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-07-02 11:36:20 +0200

    Dodat test za sistemsku operaciju VratiKnjigePoAutoruSO

--- Server.Test/KontrolerTests.cs
+++ Server.Test/KontrolerTests.cs
@@ -54,12 +54,6 @@ namespace Server.Test
                 }
             };
 
-            List<Knjiga> listaZaDodavanje = new List<Knjiga>();
-
-            listaZaDodavanje.Add(knjiga1);
-            listaZaDodavanje.Add(knjiga2);
-            listaZaDodavanje.Add(knjiga3);
-
             //upisivanje u bazu
             DodajKnjiguSO operacijaZaDodavanje = new DodajKnjiguSO();
 
@@ -92,6 +86,92 @@ namespace Server.Test
             operacijaZaBrisanje.Template(knjiga2);
             operacijaZaBrisanje.Template(knjiga3);
         }
+
+        [Fact]
+        public void Kontroler_VratiKnjigeIzBibliotekePoAutoru_ReturnListKnjiga()
+        {
+            //Act
+            //dodavanje nekoliko knjiga u bazu
+            Knjiga knjiga1 = new Knjiga
+            {
+                Naslov = "Naslov1",
+                BrojPrimeraka = 100,
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                },
+                Autor = new Autor
+                {
+                    IDAutor = 1
+                }
+            };
+
+            Knjiga knjiga2 = new Knjiga
+            {
+                Naslov = "Naslov2",
+                BrojPrimeraka = 100,
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                },
+                Autor = new Autor
+                {
+                    IDAutor = 1
+                }
+            };
+
+            Knjiga knjiga3 = new Knjiga
+            {
+                Naslov = "Naslov3",
+                BrojPrimeraka = 100,
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                },
+                Autor = new Autor
+                {
+                    IDAutor = 3
+                }
+            };
+
+            //upisivanje u bazu
+            DodajKnjiguSO operacijaZaDodavanje = new DodajKnjiguSO();
+
+            operacijaZaDodavanje.Template(knjiga1);
+            knjiga1.IDKnjiga = operacijaZaDodavanje.Rezultat;
+            operacijaZaDodavanje.Template(knjiga2);
+            knjiga2.IDKnjiga = operacijaZaDodavanje.Rezultat;
+            operacijaZaDodavanje.Template(knjiga3);
+            knjiga3.IDKnjiga = operacijaZaDodavanje.Rezultat;
+
+
+            //citanje dodatih knjiga iz baze po autoru
+            VratiKnjigePoAutoruSO operacijaZaCitanje = new VratiKnjigePoAutoruSO();
+
+            operacijaZaCitanje.Template(new Knjiga
+            {
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                },
+                Autor = new Autor
+                {
+                    ImePrezime = "Robert"
+                }
+            });
+            List<Knjiga> procitaneKnjige = operacijaZaCitanje.Rezultat;
+
+            //Assert
+            procitaneKnjige.Should().NotBeNullOrEmpty();
+            procitaneKnjige.Should().HaveCount(1);
+
+            //brisanje unetih knjiga
+            ObrisiKnjiguSO operacijaZaBrisanje = new ObrisiKnjiguSO();
+            operacijaZaBrisanje.Template(knjiga1);
+            operacijaZaBrisanje.Template(knjiga2);
+            operacijaZaBrisanje.Template(knjiga3);
+        }
+
 
     }
 }
