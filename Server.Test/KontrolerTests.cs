6868bdd582ec5534c8418e07d15345e4bff5a890
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-07-02 11:42:22 +0200

    Dodat test za sistemsku operaciju DodajKnjiguSO

--- Server.Test/KontrolerTests.cs
+++ Server.Test/KontrolerTests.cs
@@ -172,6 +172,59 @@ namespace Server.Test
             operacijaZaBrisanje.Template(knjiga3);
         }
 
+        [Fact]
+        public void Kontroler_DodajKnjigu()
+        {
+            //Act
+            //dodavanje knjige u bazu
+            Knjiga knjiga1 = new Knjiga
+            {
+                Naslov = "Naslov1",
+                BrojPrimeraka = 100,
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                },
+                Autor = new Autor
+                {
+                    IDAutor = 1
+                }
+            };
+
+            //upisivanje u bazu
+            DodajKnjiguSO operacijaZaDodavanje = new DodajKnjiguSO();
+
+            operacijaZaDodavanje.Template(knjiga1);
+            knjiga1.IDKnjiga = operacijaZaDodavanje.Rezultat;
+
+            //citanje dodate knjiga iz baze
+            VratiSveKnjigeSO operacijaZaCitanje = new VratiSveKnjigeSO();
+
+            operacijaZaCitanje.Template(new Knjiga
+            {
+                Biblioteka = new Biblioteka
+                {
+                    IDBiblioteka = 1
+                }
+            });
+
+            Knjiga procitanaKnjiga = operacijaZaCitanje.Rezultat[0];
+
+            //Assert
+            operacijaZaCitanje.Rezultat.Should().NotBeNullOrEmpty();
+            operacijaZaCitanje.Rezultat.Should().HaveCount(1);
+
+            procitanaKnjiga.IDKnjiga.Should().Be(knjiga1.IDKnjiga);
+            procitanaKnjiga.Naslov.Should().Be(knjiga1.Naslov);
+            procitanaKnjiga.BrojPrimeraka.Should().Be(knjiga1.BrojPrimeraka);
+            procitanaKnjiga.Biblioteka.IDBiblioteka.Should().Be(knjiga1.Biblioteka.IDBiblioteka);
+            procitanaKnjiga.Autor.IDAutor.Should().Be(knjiga1.Autor.IDAutor);
+
+            //brisanje unete knjige
+            ObrisiKnjiguSO operacijaZaBrisanje = new ObrisiKnjiguSO();
+            operacijaZaBrisanje.Template(knjiga1);
+        }
+
 
     }
 }
